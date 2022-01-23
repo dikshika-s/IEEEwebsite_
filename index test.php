533b6d61379c7542170298aa7e08e1a08af1965d
nishritha <b20283@students.iitmandi.ac.in>
2022-01-23 13:30:38 +0530

    Executive council images and contact details

---
+++ index test.php
@@ -0,0 +1,62 @@
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+    <img class="img img-circle" src="img/student branch/ankit.webp" height="120px" width="120px">
+    <p><strong> Ankit Karan </strong></p>
+    <div style="line-height: 90%">
+        <p>Chairperson</p>
+        <p>IEEE RAS Society</p>
+        <a href="mailto:T18081@students.iitmandi.ac.in"><i class="fas fa-envelope"
+                style="color: #4875B4"></i></a>&nbsp
+        <a href="https://www.facebook.com/profile.php?id=100008808974220" target="_blank"><i
+                class="fab fa-facebook" style="color: #3B5998"></i></a>
+        <br>
+    </div>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+    <img class="img img-circle" src="img/student branch/Abhishek_Parmar.webp" height="120px"
+        width="120px">
+    <p><strong> Abhishek Parmar </strong></p>
+    <div style="line-height: 90%">
+        <p>Vice Chairperson</p>
+        <p>IEEE RAS Society</p>
+        <a href="https://www.linkedin.com/in/neeraj-sharma-93895a14a/" target="_blank"><i
+                class="fab fa-linkedin" style="color: #4875B4;"></i></a>&nbsp
+        <a href="mailto:b17050@students.iitmandi.ac.in"><i class="fas fa-envelope"
+                style="color: #4875B4"></i></a>&nbsp
+        <a href="https://www.facebook.com/profile.php?id=100009505387539" target="_blank"><i
+                class="fab fa-facebook" style="color: #3B5998"></i></a>
+    </div>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+    <img class="img img-circle" src="img/student branch/Dhanush_chavan.webp" height="120px"
+        width="120px">
+    <p><strong> Dhanush Chavan </strong></p>
+    <div style="line-height: 90%">
+        <p>Secretary</p>
+        <p>IEEE RAS Society</p>
+        <a href="https://www.linkedin.com/in/aditi-mann-180a14105/" target="_blank"><i
+                class="fab fa-linkedin" style="color: #4875B4;"></i></a>&nbsp
+        <a href="mailto:b15205@students.iitmandi.ac.in"><i class="fas fa-envelope"
+                style="color: #4875B4"></i></a>&nbsp
+        <a href="https://www.facebook.com/profile.php?id=100010180416490" target="_blank"><i
+                class="fab fa-facebook" style="color: #3B5998"></i></a>
+        <br>
+    </div>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+    <img class="img img-circle" src="img/student branch/vinit_srivastava.webp" height="120px"
+        width="120px">
+    <p><strong> Vinit Srivastava </strong></p>
+    <div style="line-height: 90%">
+        <p>Treasurer</p>
+        <p>IEEE RAS Societyy</p>
+        <a href="https://www.linkedin.com/in/jhalak-choudhary-75921716b/" target="_blank"><i
+                class="fab fa-linkedin" style="color: #4875B4;"></i></a>&nbsp
+        <a href="mailto:b17046@students.iitmandi.ac.in"><i class="fas fa-envelope"
+                style="color: #4875B4"></i></a>&nbsp
+        <a href="https://www.facebook.com/jhalak.choudhary.92" target="_blank"><i
+                class="fab fa-facebook" style="color: #3B5998"></i></a>
+        <br>
+    </div>
